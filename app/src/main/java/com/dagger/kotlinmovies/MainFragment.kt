ad20432d9cba71ba9e91996ec54b45d895a653e3
the-dagger <harshithdwivedi@gmail.com>
2017-10-31 22:50:08 +0530

    add option to switch between top rated and popular
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/com/dagger/kotlinmovies/MainFragment.kt
+++ app/src/main/java/com/dagger/kotlinmovies/MainFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.google.gson.Gson
 import okhttp3.*
 import okhttp3.Response
@@ -17,15 +15,32 @@ import java.io.IOException
  */
 class MainFragment : Fragment() {
 
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_main, container, false)
+
+
+        recyclerView = view.findViewById(R.id.recyclerViewMovies)
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+
+        setHasOptionsMenu(true)
+        makeNetworkCall(POPULAR)
+        return view
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.action_top_rated -> makeNetworkCall(TOP_RATED)
+            else -> makeNetworkCall(POPULAR)
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun makeNetworkCall(endpoint: String) {
         val okhttpClient = OkHttpClient()
-        val request = Request.Builder().url(BASE_URL + POPULAR + "?api_key=" + API_KEY).build()
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewMovies)
-        recyclerView.layoutManager = GridLayoutManager(context,2)
-
-
+        val request = Request.Builder().url(BASE_URL + endpoint + "?api_key=" + API_KEY).build()
         okhttpClient.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call?, e: IOException?) {
 
@@ -41,7 +56,12 @@ class MainFragment : Fragment() {
             }
 
         })
-
-        return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
 }
